790b0777ce990828a94c44be90ddca1b41990fe1
ahmad <Ahmed.Bouazizi@issatmh.u-monastir.tn>
2021-04-17 20:07:57 +0100

    init

---
+++ src/scripts/main.js
@@ -0,0 +1,11 @@
+import jquery from 'jquery';
+import {Popover as Popover} from 'bootstrap';
+import * as bootstrap from 'bootstrap';
+
+import '../style.scss';
+
+const container=document.querySelector('.container');
+const btn=document.getElementById('myBtn');
+
+
+
